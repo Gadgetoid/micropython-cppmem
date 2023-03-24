a8931f40950a3f143c331608f297c57ec3869aad
Phil Howard <phil@pimoroni.com>
2023-03-24 14:25:13 +0000

    Enable optional allocation on MicroPython heap.

--- cppmem.c
+++ cppmem.c
@@ -3,12 +3,16 @@
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(cpp_mem_get_alloc_bytes_obj, cpp_mem_get_alloc_bytes);
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(cpp_mem_get_alloc_count_obj, cpp_mem_get_alloc_count);
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(cpp_mem_get_free_count_obj, cpp_mem_get_free_count);
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(cpp_mem_set_allocator_mode_obj, cpp_mem_set_allocator_mode);
 
 STATIC const mp_map_elem_t cppmem_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_cppmem) },
     { MP_ROM_QSTR(MP_QSTR_alloc_bytes), MP_ROM_PTR(&cpp_mem_get_alloc_bytes_obj) },
     { MP_ROM_QSTR(MP_QSTR_alloc_count), MP_ROM_PTR(&cpp_mem_get_alloc_count_obj) },
     { MP_ROM_QSTR(MP_QSTR_free_count), MP_ROM_PTR(&cpp_mem_get_free_count_obj) },
+    { MP_ROM_QSTR(MP_QSTR_set_mode), MP_ROM_PTR(&cpp_mem_set_allocator_mode_obj) },
+    { MP_ROM_QSTR(MP_QSTR_FIXED_HEAP), MP_ROM_INT(0) },
+    { MP_ROM_QSTR(MP_QSTR_MICROPYTHON), MP_ROM_INT(1) }
 };
 STATIC MP_DEFINE_CONST_DICT(mp_module_cppmem_globals, cppmem_globals_table);
 
--- cppmem.cpp
+++ cppmem.cpp
@@ -4,12 +4,28 @@
 #include <cstring>
 #include <cstdio>
 
+enum allocator_mode {
+    FIXED_HEAP = 0,
+    MICROPYTHON
+};
+
 static uint32_t alloc_bytes = 0;
 static uint32_t alloc_count = 0;
 static uint32_t free_count = 0;
 
+static allocator_mode mode = FIXED_HEAP;
+static constexpr size_t cpp_heap_size = 10 * 1024 / 4;
+static uint32_t cpp_heap[cpp_heap_size];
+static uint32_t ptr = 0;
+static char cpp_err_buf[128] = {0};
+
 extern "C" {
 #include "cppmem.h"
+
+mp_obj_t cpp_mem_set_allocator_mode(mp_obj_t new_mode) {
+    mode = (allocator_mode)mp_obj_get_int(new_mode);
+    return mp_const_none;
+}
 
 mp_obj_t cpp_mem_get_alloc_bytes() {
     return mp_obj_new_int(alloc_bytes);
@@ -23,12 +39,15 @@ mp_obj_t cpp_mem_get_free_count() {
     return mp_obj_new_int(free_count);
 }
 
+void mpy_free(void *p) {
+    return m_free(p);
 }
 
-static constexpr size_t cpp_heap_size = 10 * 1024 / 4;
-static uint32_t cpp_heap[cpp_heap_size];
-static uint32_t ptr = 0;
-static char cpp_err_buf[128] = {0};
+void* mpy_malloc(size_t n) {
+    return m_malloc(n);
+}
+
+}
 
 std::size_t alloc_size(std::size_t n) {
     std::size_t s = n / 4;
@@ -37,6 +56,9 @@ std::size_t alloc_size(std::size_t n) {
 }
 
 void* stat_new(std::size_t n) {
+    if(mode == MICROPYTHON) {
+        return mpy_malloc(n);
+    }
     std::size_t s = alloc_size(n);
     if(ptr + s > cpp_heap_size) {
         snprintf(cpp_err_buf, sizeof(cpp_err_buf), "Failed to allocate %d bytes.", s * 4);
@@ -53,6 +75,9 @@ void* stat_new(std::size_t n) {
 }
 
 void stat_free(void *p) {
+    if(mode == MICROPYTHON) {
+        return mpy_free(p);
+    }
     free_count ++;
     //return std::free(p);
     return;
